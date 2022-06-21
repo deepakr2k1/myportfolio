d662b10b7c5b55286aabb5e7c1d159ec846c56ed
Deepak Rathore <deepakrathore2k1@gmail.com>
2022-06-22 01:09:55 +0530

    updated UI

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -24,10 +24,6 @@ function Footer() {
                     <a href="#projects"><Tag>Projects</Tag></a>
                 </div>
                 <SocialLink/>
-                <p className="footerText">+91 9755827007</p>
-                <p className="footerText">deepakrathore2k1@gmail.com</p>
-                <p className="footerText">Sharma farm road, Hazira, Gwalior</p>
-                <p className="footerText">Pincode - 474003</p>
                 <p className="footerText">Designed & Built by Deepak Rathore 2021<sup>&#169;</sup></p>
             </div>
         </footer>
