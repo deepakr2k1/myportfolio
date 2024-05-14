ad224a540193cc7c60150bcfe1b145c4cf696d6b
deepakr2k1 <dev.deepakr2k1@gmail.com>
2024-05-14 11:09:31 +0200

    Updated the About section

--- src/components/about me/index.js
+++ src/components/about me/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import H1 from '../UI/H1'
-import H2 from '../UI/H2'
+import React from 'react';
+import H1 from '../UI/H1';
+import H2 from '../UI/H2';
 import DeveloperSVG from '../../assets/developer_2.svg';
 import "react-circular-progressbar/dist/styles.css";
-import './style.css'
+import './style.css';
 
 export default function AboutMe() {
     return (
@@ -12,24 +12,25 @@ export default function AboutMe() {
             <H2>What I DO</H2>
             <div className="aboutMe">
                 <div class="aboutMeLeft">
-                    <p>Hello! My name is Deepak Rathore and I'm passionate about programming & web-dev.
-                        I love to work on real world projects and also loves to reaching out the new people
-                        and finding imaginative ways to communicate ideas.</p>
+                    <p>Hey there! I'm Deepak Rathore, an avid programmer and web developer with a fervent enthusiasm for real-world projects.
+                        I thrive on the collaborative process, always seeking imaginative ways to communicate ideas and connect with new people.</p>
 
-                    <p style={{ marginTop: 6 }}>I had worked as SDE Intern at
-                        <a style={{ color: "#16c79a" }} target="noreferrer" href="https://www.dunzo.com/"> Dunzo </a> ,
-                        <a style={{ color: "#16c79a" }} target="noreferrer" href="https://www.practo.com/"> Practo </a> ,
-                        <a style={{ color: "#16c79a" }} target="noreferrer" href="https://www.linkedin.com/company/fan-inc./about/"> Fans Inc. </a> ,
-                        <a style={{ color: "#16c79a" }} target="noreferrer" href="https://www.klimb.io/"> Klimb.io</a>.
-                        I got to work on the amazing projects with great team members.
-                        I worked on different software profiles includes Full Stack Developer, Backend Developer and iOS Developer.
-                        and different technologies includes React, Angular, Node JS, ios development (swift), MySql, MongoDB, AWS.
-                    </p>
+                    <p>My journey has taken me through esteemed companies like
+                        <a style={ { color: "#16c79a" } } target="noreferrer" href="https://www.amazon.de/"> Amazon </a> ,
+                        <a style={ { color: "#16c79a" } } target="noreferrer" href="https://www.deutsche-bank.de/"> Deutsche Bank </a> ,
+                        <a style={ { color: "#16c79a" } } target="noreferrer" href="https://www.dunzo.com/"> Dunzo </a> ,
+                        <a style={ { color: "#16c79a" } } target="noreferrer" href="https://www.practo.com/"> Practo </a> ,
+                        where I've had the privilege
+                        to contribute to incredible projects alongside talented team members. I've worn various hats, from Full Stack Developer to
+                        Backend Developer and iOS Developer, delving into a diverse array of technologies including Spring Boot, React, Angular, Node.js, iOS
+                        development (Swift), Postgres, MySQL, MongoDB, and AWS.</p>
+
+                    <p>Let's create something amazing together! Feel free to explore my portfolio to learn more about my work and experiences.</p>
                 </div>
                 <div className="aboutMeRight">
-                    <img width="100%" height="fit-content" src={DeveloperSVG} alt="Developer PNG" />
+                    <img width="100%" height="fit-content" src={ DeveloperSVG } alt="Developer PNG" />
                 </div>
             </div>
         </div>
-    )
+    );
 }
