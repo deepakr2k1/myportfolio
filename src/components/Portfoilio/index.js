164e14965b7d0f053dadb272a88efcde28e8049f
deepakr2k1 <dev.deepakr2k1@gmail.com>
2024-05-23 01:11:17 +0200

    Updated Projects section

--- src/components/Portfoilio/index.js
+++ src/components/Portfoilio/index.js
@@ -1,59 +1,59 @@
-import React from 'react'
-import Project from './project'
-import H1 from '../UI/H1'
-import H2 from '../UI/H2'
+import React from 'react';
+import Project from './project';
+import H1 from '../UI/H1';
+import H2 from '../UI/H2';
 
 export default function Portfolio() {
     return (
-        <div id="projects" className="container" style={{ marginTop: "30px" }}>
+        <div id="projects" className="container" style={ { marginTop: "30px" } }>
             <H1>Projects</H1>
-            <H2 styles={{ marginTop: "32px", textAlign: "center" }}>Some Things I’ve Built</H2>
-            <div className="text-center" style={{ marginTop: 24 }}>
+            <H2 styles={ { marginTop: "32px", textAlign: "center" } }>Some Things I’ve Built</H2>
+            <div className="text-center" style={ { marginTop: 24 } }>
                 <Project
-                    title="Ping pong Multiplayer browser game"
-                    desc={[
-                        "An online browser game where multiple users can play games at the same time & also create their own room for a friendly match.",
-                        "It is built with socket.io and node.js for Backend, & further uses the P5.js library for drawing game objects."
-                    ]}
-                    tags={[
-                        "Node.js", "Express", "Socket.io", "P5.js"
-                    ]}
-                    links={{ git: "https://github.com/deepakrathore2k1/Multiplayer-online-ping-pong-game", online: "https://online-ping-pong-game.herokuapp.com/" }}
+                    title="CP Edge case Finder"
+                    desc={ [
+                        "Created a web app to generate targeted test cases from corrected code & validate against programmers code.",
+                        "Built a multithreaded app with Java Spring Boot and React, supporting C++, Java, and Python enhancing speed and user experience."
+                    ] }
+                    tags={ [
+                        "Spring Boot", "Java", "Multithreading", "React"
+                    ] }
+                    links={ { git: "https://github.com/deepakr2k1/cp-edge-case-finder", online: "#" } }
                 />
                 <Project
                     title="Tourist’s Blog Website"
-                    desc={[
+                    desc={ [
                         "An online website to read & create tourism blogs, It includes User Authentication system for the users (login/register)",
                         "Backend is created using Node.js, Express.js, and used MongoDB for the database. Its supports CRUD operations."
-                    ]}
-                    tags={[
-                        "Node.js", "Express", "MongoDB", "User Auth"
-                    ]}
-                    links={{ git: "https://github.com/deepakrathore2k1/Tourist-s-Blogs", online: "https://tourists-blogs.herokuapp.com/blogs" }}
+                    ] }
+                    tags={ [
+                        "Node.js", "RabbitMQ", "Authentication", "Caching"
+                    ] }
+                    links={ { git: "https://github.com/deepakr2k1/tourism-blogs", online: "#" } }
+                />
+                <Project
+                    title="Instant Bot"
+                    desc={ [
+                        "A desktop application to create Bots that can trigger different repetitive GUI events to finish repetitive tasks quickly",
+                        "It helps user to perform repetitive task with low efforts."
+                    ] }
+                    tags={ [
+                        "Eel", "Python", "PyAutoGUI", "HTML/CSS"
+                    ] }
+                    links={ { git: "https://github.com/deepakr2k1/Instant-Bot", online: "#" } }
                 />
                 <Project
                     title="House Price Prediction API"
-                    desc={[
+                    desc={ [
                         "It predicts the prices of houses (Bangalore), based on different features like BHK, Location, etc.",
                         "Linear Regression algorithm is used for model training, FastAPI is used as a framework for API and hosted at Heroku."
-                    ]}
-                    tags={[
+                    ] }
+                    tags={ [
                         "Fast API", "Machine Learning", "Python", "Heroku"
-                    ]}
-                    links={{ git: "https://github.com/deepakrathore2k1/House-Price-Prediction-API", online: "https://hpp-api.herokuapp.com/docs" }}
-                />
-                <Project
-                    title="Add data via Excel"
-                    desc={[
-                        "An online website to Insert data of candidates to MongoDB database via uploading excel file(.xlsx or .xls) on landing page",
-                        "Backend is created using Node.js, Express.js, & multer is used handling file upload & deletion."
-                    ]}
-                    tags={[
-                        "Node.js", "Express", "Multer"
-                    ]}
-                    links={{ git: "https://github.com/deepakrathore2k1/inserting-data-via-excel", online: "https://inserting-data-via-excel.herokuapp.com/" }}
+                    ] }
+                    links={ { git: "https://github.com/deepakr2k1/House-Price-Prediction-API", online: "#" } }
                 />
             </div>
         </div>
-    )
+    );
 }
